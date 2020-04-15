00fe1b9fe18ef4a76d63f2506d5e769f9cf39436
krejnerw <weronika.krejner@hotmail.com>
2020-04-15 23:43:40 +0200

    edit2

--- L4_cw/zad_5.py
+++ L4_cw/zad_5.py
@@ -7,8 +7,8 @@ class ciagi:
     def wyswietl_dane(self):
         return self.a1, self.r, self.n
 
-    def pobierz_elementy(self,a1,*elementy):
-        self.a1=a1
+    def pobierz_elementy(self,*elementy):
+        self.a1=elementy[0]
         self.elementy=elementy
 
     def pobierz_parametry(self,a1,r,n):
@@ -19,15 +19,15 @@ class ciagi:
     def policz_sume(self):
         if self.n!=0:
             return ((((2*self.a1+((self.n-1)*self.r))*0.5))*self.n)
-        return sum(self.elementy)+self.a1
+        return sum(self.elementy)
     #???
     def policz_elementy(self):
         if self.n!=0:
-            #lista = [self.a1+self.r*i for i in range(self.n)]
-            #return lista
-            return self.n
-        return (len(self.elementy)+self.a1)
-
+            lista = [self.a1+self.r*i for i in range(self.n)]
+            return lista
+        return [self.elementy[i]for i in range(len(self.elementy))]
+            
+        
 
 ciag = ciagi()
 
@@ -36,5 +36,6 @@ ciag.pobierz_elementy(1.5,2.5,3.5)
 print(ciag.policz_elementy())
 print(ciag.policz_sume())
 ciag.pobierz_parametry(1.5,1,3)
+print(ciag.policz_elementy())
 print(ciag.policz_sume())
 print(ciag.wyswietl_dane())
